57f3a74f8ce724061a146bddac8854ed0f873971
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-08-26 23:58:36 +0300

    Add sample unit tests

---
+++ app_python/run_tests.py
@@ -0,0 +1,3 @@
+from tests.test_app import test_home_page
+
+test_home_page()
---
+++ app_python/tests/test_app.py
@@ -0,0 +1,20 @@
+import re
+
+import pytest
+
+from app import app
+
+
+def test_home_page():
+    """
+    GIVEN a Flask application configured for testing,
+    WHEN the '/' page is requested (GET),
+    THEN that the response is and contains the date and time.
+    """
+    with app.test_client() as test_client:
+        response = test_client.get("/")
+        assert response.status_code == 200
+        assert re.match(
+            r"<h1>The current time in Moscow is: \d\d:\d\d:\d\d - \d\d/\d\d/\d{4}</h1>",
+            response.data.decode("utf-8"),
+        )
